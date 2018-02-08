92de0861b629ae1d27564f877ab8ee5c7c195605
Nytheneals <nythes@gmail.com>
2018-02-08 14:16:52 -0500

    Added React-router4

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 // let's go!
 import React from "react";
 import ReactDom from "react-dom";
+// import { BrowserRouter, Match, Miss } from "react-router";
+// import { Router, Route, Switch } from "react-router";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 
 //CSS IMPORT
 import "./css/style.css";
 
 //COMPONENT IMPORT
-import App from "./component/App";
+import StorePicker from "./components/StorePicker";
+import App from "./components/App";
+import NotFound from "./components/NotFound";
+// CREATING ROUTES
 
-ReactDom.render(<App />, document.querySelector("#main"));
+const Root = () => {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={StorePicker} />
+          <Route path="/store/:storeId" component={App} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+ReactDom.render(<Root />, document.querySelector("#main"));
