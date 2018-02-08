156620cbe17ef321720d43243b8db72cd3a89fc7
Nytheneals <nythes@gmail.com>
2018-02-08 09:39:54 -0500

    Created store Picker Component

---
+++ src/component/StorePicker.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import ReactDom from "react-dom";
+
+class StorePicker extends Component {
+  render() {
+    return (
+      <div>
+        <form className="store-selector">
+          <h2>Please Enter Store</h2>
+          <input type="text" required placeholder="Store Name" />
+          <button type="submit">Visit Store --></button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default StorePicker;
