e9c30c24422e1f2f38b3582c0520d2406482d9e6
Nytheneals <nythes@gmail.com>
2018-02-08 09:40:34 -0500

    Added CSS/Imported

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 // let's go!
+import React, { Component } from "react";
+import ReactDom from "react-dom";
+
+//CSS IMPORT
+import "./css/style.css";
+
+//COMPONENT IMPORT
+import StorePicker from "./component/StorePicker";
+
+ReactDom.render(<StorePicker />, document.querySelector("#main"));
